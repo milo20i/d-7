53223ea8d65402aaae92b03f50802bcc211e0a32
Danilo De Matteis <62311664+milo20i@users.noreply.github.com>
2024-08-15 10:25:30 +0200

    Add files via upload
    a program which reads the number of blocks the builders have, and outputs the height of the pyramid that can be built using these blocks

---
+++ main.py
@@ -0,0 +1,10 @@
+blocks = int(input("Enter the number of blocks: "))
+
+height = 0
+used_blocks = 0
+
+while used_blocks + (height + 1) <= blocks:
+    height += 1
+    used_blocks += height
+
+print("The height of the pyramid:", height)
